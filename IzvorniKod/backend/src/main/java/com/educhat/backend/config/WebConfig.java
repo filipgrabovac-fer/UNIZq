d639a257d88a6e63cc67567155f0f02924840d68
Hrvoje8 <hrvoje.dudjak@fer.unizg.hr>
2024-10-28 14:45:04 +0100

    Configure database connection

--- IzvorniKod/backend/src/main/java/com/educhat/backend/config/WebConfig.java
+++ IzvorniKod/backend/src/main/java/com/educhat/backend/config/WebConfig.java
@@ -1,5 +1,8 @@
 package com.educhat.backend.config;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,5 +12,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/{path:[^\\.]*}")
                 .setViewName("forward:/index.html");
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
     }
 }
