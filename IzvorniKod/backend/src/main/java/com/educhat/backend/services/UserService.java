abef26ab958755237dd86022dd825384ccd0ee91
Matej Stupljanec <matej.stupljanec@fer.unizg.hr>
2024-10-30 19:05:11 +0100

    add endpoint to get user data by userId

--- IzvorniKod/backend/src/main/java/com/educhat/backend/services/UserService.java
+++ IzvorniKod/backend/src/main/java/com/educhat/backend/services/UserService.java
@@ -44,4 +44,8 @@ public class UserService {
         }
         return user;
     }
+
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+    }
 }
