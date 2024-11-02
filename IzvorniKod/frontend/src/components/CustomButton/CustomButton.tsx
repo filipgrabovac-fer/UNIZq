3300e6548076f83eaab2f09dd866f0c25ac16e86
Matija <matija.pticar@fer.hr>
2024-11-02 09:45:38 +0100

    dodana opcionalna funkcija onClick,promjenjene sitnice u css-u

--- IzvorniKod/frontend/src/components/CustomButton/CustomButton.tsx
+++ IzvorniKod/frontend/src/components/CustomButton/CustomButton.tsx
@@ -1,19 +1,18 @@
-import { Dispatch, SetStateAction, useState } from "react";
-import { CustomInput } from "../CustomInput/CustomInput";
 type CustomInputType = {
   variant_?: string;
+  onClick?: () => void;
 };
 export const CustomButton = ({ variant_ }: CustomInputType) => {
   const variant = variant_?.toLocaleLowerCase();
   if (variant === "cancel") {
     return (
-      <button className="w-full h-[38] rounded bg-withe border-solid border-2 border-black">
+      <button className="w-full h-[38] rounded bg-withe border-solid border-2 border-black hover:scale-110 hover:opacity-0.75">
         cancel
       </button>
     );
   } else if (variant === "logout") {
     return (
-      <button className="w-full h-[38] rounded bg-primary border-solid border-2 border-black bg-primary text-white border-gray_border ">
+      <button className="w-full h-[38] rounded bg-primary border-solid border-2 border-black bg-primary text-white border-gray_border hover:scale-110 hover:opacity-75 ">
         logout
       </button>
     );
