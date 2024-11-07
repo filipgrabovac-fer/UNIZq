eaea148fae10025a3246a56702718de7873e00f0
Filip Grabovac <filip.grabovac@fer.unizg.hr>
2024-11-07 19:48:49 +0100

    remove unused imports

--- IzvorniKod/frontend/src/pages/Home/Home.tsx
+++ IzvorniKod/frontend/src/pages/Home/Home.tsx
@@ -1,32 +1,9 @@
-import { CustomInput } from "../../components/CustomInput/CustomInput";
-import { CustomLoginRegisterButton } from "../../components/CustomLoginRegisterButton/CustomLoginRegisterButton.component";
 import { SidebarLayout } from "../../layouts/SidebarLayout/SidebarLayout";
 
 export const Home = () => {
   return (
     <SidebarLayout>
-      <div className="w-full">
-        <CustomLoginRegisterButton
-          title="123"
-          onClick={() => console.log(123)}
-        />
-        <img src="/images/img123.png" />
-
-        <form action="">
-          <CustomInput
-            title="123"
-            placeholder="123"
-            setValue={() => 0}
-            required={true}
-            type="email"
-          />
-          <CustomLoginRegisterButton
-            title="Register"
-            onClick={() => 0}
-            type="submit"
-          />
-        </form>
-      </div>
+      <div className="w-full"></div>
     </SidebarLayout>
   );
 };
