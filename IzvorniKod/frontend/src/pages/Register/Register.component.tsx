2bc822aa986bd664543bb1de857862246d0a4de1
Matija <matija.pticar@fer.hr>
2024-11-07 19:36:13 +0100

    Registar page

--- IzvorniKod/frontend/src/pages/Register/Register.component.tsx
+++ IzvorniKod/frontend/src/pages/Register/Register.component.tsx
@@ -20,29 +20,35 @@ export const Register = () => {
         className="mb-8 w-[330px]"
       />
 
-      <form className=" min-[400px]:w-[320px] w-[250px] mb-4" method="POST">
+      <form
+        className="flex flex-col gap-3 min-[400px]:w-[320px] w-[250px] mb-4"
+        action=""
+      >
         <CustomInput
+          required={true}
           title="username"
           placeholder="add username"
           setValue={setUsername}
         />
         <CustomInput
+          required={true}
           title="email"
           placeholder="add email"
           setValue={setEmail}
         />
         <CustomInput
+          required={true}
           title="password"
           placeholder="add password"
           setValue={setPassword}
         />
-      </form>
-      <div className="w-[381px] h-[60px] p-[15px]">
+
         <CustomLoginRegisterButton
+          type="submit"
           title="Register"
           onClick={() => 0}
-        ></CustomLoginRegisterButton>
-      </div>
+        />
+      </form>
     </div>
   );
 };
