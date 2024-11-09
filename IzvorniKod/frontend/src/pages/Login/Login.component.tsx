b1e1175f4e6c1405adefe0d79f5f504199c5ca56
Filip Grabovac <filip.grabovac@fer.unizg.hr>
2024-11-09 19:04:22 +0100

    change route on login and register page when logging in with google

--- IzvorniKod/frontend/src/pages/Login/Login.component.tsx
+++ IzvorniKod/frontend/src/pages/Login/Login.component.tsx
@@ -50,9 +50,7 @@ export const Login = () => {
 
       <a
         className="mt-2"
-        href={`${
-          process.env.DEV && "http://localhost:8080"
-        }/oauth2/authorization/google`}
+        href={`${process.env.DEV && "http://localhost:8080"}/api/auth/google`}
       >
         <GoogleButton />
       </a>
