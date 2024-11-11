aaee618b625a0d739dae1530ecd95f9dd504c293
sucicAnamarija <anamarija.sucic@fer.unizg.hr>
2024-11-11 18:53:39 +0100

    Made post preview component

--- IzvorniKod/frontend/src/pages/Home/Home.tsx
+++ IzvorniKod/frontend/src/pages/Home/Home.tsx
@@ -1,6 +1,11 @@
 import { FacultySubject } from "../../components/FacultySubject/FacultySubject";
+import { PostPreview } from "../../components/PostPreview/PostPreview";
+
 
 export const Home = () => {
   return <div className="w-full bg-secondary">Home page
-  <FacultySubject subjectTitle="Operative Systems" subjectDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt..."/></div>;
+  <FacultySubject subjectTitle="Operative Systems" subjectDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt..."/>
+  <PostPreview postTitle="efjenjkhwkbjckcbhkbjhbhnrjbvhrbvrkvkrekefjenjkhwkbjckcbhkbjhbhnrjbvhrbvrkvkrek"/>
+  </div>;
+  
 };
