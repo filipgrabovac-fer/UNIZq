0bfc8994fc45a60d221fad0b0bfd95f208fa11a7
sucicAnamarija <anamarija.sucic@fer.unizg.hr>
2024-11-11 09:40:42 +0100

    Added responsive design to faculty subject commponent

--- IzvorniKod/frontend/src/components/FacultySubject/FacultySubject.tsx
+++ IzvorniKod/frontend/src/components/FacultySubject/FacultySubject.tsx
@@ -10,10 +10,10 @@ export const FacultySubject = ({
   onClick,
 }: FacultySubjectType) => {
   return (
-    <div onClick={onClick} className="w-[250px] h-[250px] bg-white rounded-[20px] p-[15px]">
-      <p className="font-bold text-[26px] mb-[10px]">{subjectTitle}</p>
+    <div onClick={onClick} className="w-[150px] h-[150px] bg-white rounded-[20px] p-[10px] min-[501px]:w-[250px] min-[501px]:h-[250px] min-[501px]:p-[15px]">
+      <p className="font-bold text-[18px] mb-[5px] min-[501px]:text-[26px] min-[501px]:mb-[10px]">{subjectTitle}</p>
       <hr />
-      <p className="mt-[10px] text-[14px]">{subjectDescription}</p>
+      <p className="mt-[5px] text-[10px] min-[501px]:text-[14px] min-[501px]:mt-[10px]">{subjectDescription}</p>
     </div>
   );
 };
--- IzvorniKod/frontend/src/pages/Home/Home.tsx
+++ IzvorniKod/frontend/src/pages/Home/Home.tsx
@@ -2,5 +2,5 @@ import { FacultySubject } from "../../components/FacultySubject/FacultySubject";
 
 export const Home = () => {
   return <div className="w-full bg-secondary">Home page
-  <FacultySubject subjectTitle="Operative Systems" subjectDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."/></div>;
+  <FacultySubject subjectTitle="Operative Systems" subjectDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt..."/></div>;
 };
