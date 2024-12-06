010730db3361dbc269c16aecb8eec42dc7293c0a
sucicAnamarija <anamarija.sucic@fer.unizg.hr>
2024-12-06 17:33:39 +0100

    Edited filter by div

--- IzvorniKod/frontend/src/components/Search/Search.tsx
+++ IzvorniKod/frontend/src/components/Search/Search.tsx
@@ -1,63 +1,61 @@
 import { CustomButton } from "../../components/CustomButton/CustomButton";
-import { Button, Popover } from 'antd';
-import { ChevronDownIcon } from "@heroicons/react/24/solid"; 
-import {MagnifyingGlassIcon} from "@heroicons/react/24/solid";
+import { Button, Popover } from "antd";
+import { ChevronDownIcon } from "@heroicons/react/24/solid";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 
 type SearchType = {
-    withAddPost: boolean;
-    onClick: () => void;
+  withAddPost: boolean;
+  onClick: () => void;
+};
+
+export const Search = ({ withAddPost, onClick }: SearchType) => {
+  const content = (
+    <div className="text-[16px]">
+      <p>Name</p>
+      <p>Date</p>
+      <p>Content</p>
+    </div>
+  );
+
+  const [searchContent, setsearchContent] = useState("");
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setsearchContent(e.target.value);
   };
-  
-  export const Search = ({
-    withAddPost,
-    onClick,
-  }: SearchType) => {
-    const content = (
-      <div className="text-[16px]">
-        <p>Name</p>
-        <p>Date</p>
-        <p>Content</p>
+
+  return (
+    <div onClick={onClick} className="h-[50px] flex  justify-evenly ">
+      <div className="p-[5px] w-[50%] rounded-[20px] bg-white flex  items-center gap-3">
+        <MagnifyingGlassIcon className="h-6 text-black" />
+        <div className="h-[80%] border-l-[1px] border-black"></div>
+        <input
+          type="text"
+          value={searchContent}
+          onChange={handleSearchChange}
+          placeholder="Search..."
+          className="h-full focus:outline-none"
+        />
       </div>
-    );
 
-    const [searchContent, setsearchContent] = useState("");
-
-    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setsearchContent(e.target.value);
-    };
-
-    return (
-      <div
-        onClick={onClick}
-        className="h-[50px] flex  justify-evenly ">
-            <div className="p-[5px] w-[50%] rounded-[20px] bg-white flex  items-center gap-3">
-              <MagnifyingGlassIcon className="h-6 text-black" />
-              <div className="h-[80%] border-l-[1px] border-black"></div>
-                <input
-                  type="text"
-                  value={searchContent}
-                  onChange={handleSearchChange}
-                  placeholder="Search..."
-                  className= "h-full focus:outline-none"/>
-            </div>
-
-            <div className="rounded-[20px]">
-                  <Popover content={content} trigger="hover">
-                  <Button className="bg-white text-black rounded-[20px] h-full text-[18px] flex items-center gap-3">
-                       <p>Filter by</p>
-                       <ChevronDownIcon className="h-5 w-5 text-black" />
-                    </Button>
-                  </Popover>
-            </div>
-
-            {withAddPost && (
-                <div className="h-full flex">
-                    <CustomButton variant="primary" title="Add post" onClick={() => {}} />
-                </div>
-            )}
-
+      <div className="rounded-[20px] border-none text-black hover:border-none hover:text-black">
+        <Popover
+          content={content}
+          trigger="hover"
+          className="border-none text-black hover:text-black"
+        >
+          <Button className="bg-white text-black rounded-[20px] h-full text-[18px] flex items-center gap-3 hover:border-none hover:text-black focus:outline-none">
+            <p className="text-black">Filter by</p>
+            <ChevronDownIcon className="h-5 w-5 text-black" />
+          </Button>
+        </Popover>
       </div>
-    );
-  };
-  
+
+      {withAddPost && (
+        <div className="h-full flex">
+          <CustomButton variant="primary" title="Add post" onClick={() => {}} />
+        </div>
+      )}
+    </div>
+  );
+};
