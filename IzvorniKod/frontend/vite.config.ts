aba7d7ec31eaaf55d63be0334a0997c3ed1a0101
Filip Grabovac <filip.grabovac@fer.unizg.hr>
2024-12-15 00:35:49 +0100

    add changes to dockerfile so it accepts env variables for frontend

--- IzvorniKod/frontend/vite.config.ts
+++ IzvorniKod/frontend/vite.config.ts
@@ -1,18 +1,16 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    define: {
-      "process.env.DEV": JSON.stringify(env.DEV),
-    },
-    plugins: [react()],
-    build: {
-      outDir: "./src/main/resources/static",
-      empyOutDir: true,
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "./src/main/resources/static",
+    emptyOutDir: true,
+  },
+  define: {
+    "process.env.VITE_DEV": JSON.stringify(process.env.VITE_DEV),
+    "process.env.VITE_GOOGLE_MAPS_API_KEY": JSON.stringify(
+      process.env.VITE_GOOGLE_MAPS_API_KEY
+    ),
+  },
 });
