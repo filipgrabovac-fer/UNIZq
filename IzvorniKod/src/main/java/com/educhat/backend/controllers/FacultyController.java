57c99b68780e0d291ee12c2adf1fe2ee601b0c9f
Matej Stupljanec <matej.stupljanec@fer.unizg.hr>
2024-12-14 16:45:30 +0100

    BE endpoint for all faculties

---
+++ IzvorniKod/src/main/java/com/educhat/backend/controllers/FacultyController.java
@@ -0,0 +1,31 @@
+package com.educhat.backend.controllers;
+
+import com.educhat.backend.models.Faculty;
+import com.educhat.backend.services.FacultyService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("*")
+public class FacultyController {
+
+    private final FacultyService facultyService;
+
+    public FacultyController(FacultyService facultyService) {
+        this.facultyService = facultyService;
+    }
+
+    @GetMapping("/faculties")
+    public ResponseEntity<List<Faculty>> getAllFaculties() {
+        List<Faculty> faculties = facultyService.getAllFaculties();
+        return ResponseEntity.ok(faculties);
+    }
+
+
+}
---
+++ IzvorniKod/src/main/java/com/educhat/backend/services/FacultyService.java
@@ -0,0 +1,19 @@
+package com.educhat.backend.services;
+
+import com.educhat.backend.models.Faculty;
+import com.educhat.backend.repository.FacultyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FacultyService {
+
+    private final FacultyRepository facultyRepository;
+
+    public List<Faculty> getAllFaculties() {
+        return facultyRepository.findAll();
+    }
+}
