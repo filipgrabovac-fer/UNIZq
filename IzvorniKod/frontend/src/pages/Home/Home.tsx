9df7f886c5c55ae28172e18216a98957d6d56f6a
Filip Grabovac <filip.grabovac@fer.unizg.hr>
2024-12-16 00:11:00 +0100

    rebase with new dev

--- IzvorniKod/frontend/src/pages/Home/Home.tsx
+++ IzvorniKod/frontend/src/pages/Home/Home.tsx
@@ -1,18 +1,15 @@
-import { FacultySubject } from "../../components/FacultySubject/FacultySubject";
 import { PostPreview } from "../../components/PostPreview/PostPreview";
 
 export const Home = () => {
   return (
     <div className=" bg-secondary overflow-hidden">
       Home page
-      <FacultySubject
-        subjectTitle="Operative Systems"
-        subjectDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt..."
-      />
       <PostPreview
-        postTitle="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe fugit at
-        ut itaque a, deleniti maiores assumenda veritatis exercitationem qui quo
-        vel, quis nulla facere minus ea sapiente cupiditate earum!"
+        postTitle="Post title"
+        onClick={() => {}}
+        canDelete={true}
+        canModify={true}
+        postID={1}
       />
     </div>
   );
