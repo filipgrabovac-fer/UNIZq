ae0c11e4e5f304662e560a2f3207acf178e6e411
sucicAnamarija <anamarija.sucic@fer.unizg.hr>
2024-12-21 13:43:08 +0100

    Made initial Sidebar, placed it to the SidebarLayput.tsx

---
+++ IzvorniKod/frontend/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,49 @@
+import { sidebarDataMock } from "../../components/Header/Header";
+import { useState } from "react";
+import type { MenuProps } from "antd";
+import { ConfigProvider, Menu } from "antd";
+
+type SidebarProps = {
+  onClick: MenuProps["onClick"];
+};
+
+export const Sidebar = ({ onClick }: SidebarProps) => {
+  const [current, setCurrent] = useState("1");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const handleClick: MenuProps["onClick"] = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
+
+  return (
+    <div>
+      <ConfigProvider
+        theme={{
+          components: {
+            Menu: {
+              colorText: "black",
+              fontSize: 16,
+              itemSelectedColor: "black",
+              itemSelectedBg: "#ececec",
+            },
+          },
+        }}
+      >
+        <Menu
+          onClick={handleClick}
+          style={{
+            width: 256,
+            borderRadius: "20px",
+          }}
+          selectedKeys={[current]}
+          mode="inline"
+          items={sidebarDataMock}
+          onSelect={() => {
+            setIsSidebarOpen(false); // dodati funkcionalnost klika na pojedinu godinu
+          }}
+        />
+      </ConfigProvider>
+    </div>
+  );
+};
--- IzvorniKod/frontend/src/pages/Home/Home.tsx
+++ IzvorniKod/frontend/src/pages/Home/Home.tsx
@@ -1,16 +1,3 @@
-import { PostPreview } from "../../components/PostPreview/PostPreview";
-
 export const Home = () => {
-  return (
-    <div className=" bg-secondary overflow-hidden">
-      Home page
-      <PostPreview
-        postTitle="Post title"
-        onClick={() => {}}
-        canDelete={true}
-        canModify={true}
-        postID={1}
-      />
-    </div>
-  );
+  return <div className=" bg-secondary overflow-hidden">Home page</div>;
 };
