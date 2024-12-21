fa2fc32e43cb4277348d6b1acfdf820e0a128299
sucicAnamarija <anamarija.sucic@fer.unizg.hr>
2024-12-21 13:43:08 +0100

    Made initial Sidebar, placed it to the SidebarLayput.tsx

--- IzvorniKod/frontend/src/layouts/SidebarLayout/SidebarLayout.tsx
+++ IzvorniKod/frontend/src/layouts/SidebarLayout/SidebarLayout.tsx
@@ -1,7 +1,7 @@
-import { Menu } from "antd";
 import { useState } from "react";
-import { Header, sidebarDataMock } from "../../components/Header/Header";
+import { Header } from "../../components/Header/Header";
 import { Outlet } from "@tanstack/react-router";
+import { Sidebar } from "../../components/Sidebar/Sidebar";
 
 export const SidebarLayout = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -14,19 +14,12 @@ export const SidebarLayout = () => {
       />
       {isSidebarOpen ? (
         <div className={"absolute left-0 top-[60px] w-full h-full bg-white"}>
-          <Menu
-            mode="inline"
-            items={sidebarDataMock}
-            defaultOpenKeys={["faculties"]}
-            onSelect={() => {
-              setIsSidebarOpen(false); // dodati funkcionalnost klika na pojedinu godinu
-            }}
-          />
+          <Sidebar onClick={undefined} />
         </div>
       ) : (
         <div className="flex w-full">
           <div className="w-[300px] max-[500px]:hidden">
-            This is the sidebar
+            <Sidebar onClick={undefined} />
           </div>
           <Outlet />
         </div>
