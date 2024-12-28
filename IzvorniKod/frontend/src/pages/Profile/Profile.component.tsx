0f77d921281f90be8fdf4bdea77793930fcbdd05
sucicAnamarija <anamarija.sucic@fer.unizg.hr>
2024-12-28 10:10:08 +0100

    Added modal when clicking on trash icon on faculty card

--- IzvorniKod/frontend/src/pages/Profile/Profile.component.tsx
+++ IzvorniKod/frontend/src/pages/Profile/Profile.component.tsx
@@ -11,6 +11,7 @@ import {
   Card,
   Form,
   Input,
+  Modal,
 } from "antd";
 import {
   UserIcon,
@@ -47,91 +48,132 @@ const data = [
   },
 ];
 
-const items: TabsProps["items"] = [
-  {
-    key: "1",
-    label: "My faculties",
-    children: (
-      <List
-        grid={{
-          gutter: 30,
-        }}
-        dataSource={data}
-        renderItem={(item) => (
-          <List.Item
-          /*actions*/
-          >
-            <Card
-              className="w-[70%] h-[170px] transition-transform duration-300 hover:scale-105 hover:shadow-md"
-              title={
-                <div className="flex justify-between">
-                  <p>{item.faculty}</p>
-                  <TrashIcon className="w-5 hover:cursor-pointer transition-transform duration-300 hover:scale-105" />
-                </div>
-              }
-            >
-              <p className="line-clamp-3">{item.description}</p>
-            </Card>
-          </List.Item>
-        )}
-      />
-    ),
-  },
-  {
-    key: "2",
-    label: "My posts",
-    children: (
-      <div className="mb-3 w-[90%]">
-        <div className="mb-3 transition-transform hover:scale-y-105 duration-300 hover:shadow-md">
-          <PostPreview
-            postTitle={
-              "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo quae odit ab magnam tempora, ut excepturi error aut impedit amet libero ipsam fugiat repudiandae aspernatur unde similique nemo hic officiis"
-            }
-            postID={0}
-            canDelete={false}
-            canModify={false}
-            onClick={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
-        </div>
-        <div className="mb-3 transition-transform hover:scale-y-105 duration-300 hover:shadow-md">
-          <PostPreview
-            postTitle={
-              "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo quae odit ab magnam tempora, ut excepturi error aut impedit amet libero ipsam fugiat repudiandae aspernatur unde similique nemo hic officiis"
-            }
-            postID={0}
-            canDelete={false}
-            canModify={false}
-            onClick={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
-        </div>
-        <div className="transition-transform hover:scale-y-105 duration-300 hover:shadow-md">
-          <PostPreview
-            postTitle={
-              "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo quae odit ab magnam tempora, ut excepturi error aut impedit amet libero ipsam fugiat repudiandae aspernatur unde similique nemo hic officiis"
-            }
-            postID={0}
-            canDelete={false}
-            canModify={false}
-            onClick={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
-        </div>
-      </div>
-    ),
-  },
-];
-
 export const Profile = () => {
   const [collapsed, setCollapsed] = useState(false);
   const [selectedKey, setSelectedKey] = useState("1");
+  const [isHovered, setIsHovered] = useState(false);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+
+  const items: TabsProps["items"] = [
+    {
+      key: "1",
+      label: "My faculties",
+      children: (
+        <List
+          className="w-[90%]"
+          grid={{
+            gutter: 30,
+            column: 2,
+          }}
+          dataSource={data}
+          renderItem={(item) => (
+            <List.Item
+
+            /*actions*/
+            >
+              <Card
+                className="h-[150px] transition-transform duration-300 hover:scale-105 hover:shadow-md"
+                title={
+                  <div className="flex justify-between">
+                    <p>{item.faculty}</p>
+                    <TrashIcon
+                      onClick={showModal}
+                      className="w-5 hover:cursor-pointer transition-transform duration-300 hover:scale-105"
+                    />
+
+                    <Modal
+                      open={isModalOpen}
+                      onOk={handleOk}
+                      onCancel={handleCancel}
+                      okButtonProps={{
+                        style: {
+                          background: "#111D4A",
+                          opacity: isHovered ? 0.8 : 1,
+                        },
+                        onMouseEnter: () => setIsHovered(true),
+                        onMouseLeave: () => setIsHovered(false),
+                      }}
+                    >
+                      <p>
+                        Are you sure you want to remove this faculty from your
+                        list?
+                      </p>
+                    </Modal>
+                  </div>
+                }
+              >
+                <p className="line-clamp-2">{item.description}</p>
+              </Card>
+            </List.Item>
+          )}
+        />
+      ),
+    },
+    {
+      key: "2",
+      label: "My posts",
+      children: (
+        <div className="mb-3 w-[90%]">
+          <div className="mb-3 transition-transform hover:scale-y-105 duration-300 hover:shadow-md">
+            <PostPreview
+              postTitle={
+                "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo quae odit ab magnam tempora, ut excepturi error aut impedit amet libero ipsam fugiat repudiandae aspernatur unde similique nemo hic officiis"
+              }
+              postID={0}
+              canDelete={false}
+              canModify={false}
+              onClick={function (): void {
+                throw new Error("Function not implemented.");
+              }}
+            />
+          </div>
+          <div className="mb-3 transition-transform hover:scale-y-105 duration-300 hover:shadow-md">
+            <PostPreview
+              postTitle={
+                "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo quae odit ab magnam tempora, ut excepturi error aut impedit amet libero ipsam fugiat repudiandae aspernatur unde similique nemo hic officiis"
+              }
+              postID={0}
+              canDelete={false}
+              canModify={false}
+              onClick={function (): void {
+                throw new Error("Function not implemented.");
+              }}
+            />
+          </div>
+          <div className="transition-transform hover:scale-y-105 duration-300 hover:shadow-md">
+            <PostPreview
+              postTitle={
+                "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo quae odit ab magnam tempora, ut excepturi error aut impedit amet libero ipsam fugiat repudiandae aspernatur unde similique nemo hic officiis"
+              }
+              postID={0}
+              canDelete={false}
+              canModify={false}
+              onClick={function (): void {
+                throw new Error("Function not implemented.");
+              }}
+            />
+          </div>
+        </div>
+      ),
+    },
+  ];
 
   const renderContent = () => {
     switch (selectedKey) {
