43b85f46bff1e0b43ad3c8fdeffe869fcea6cb53
Filip Grabovac <filip.grabovac@fer.unizg.hr>
2024-12-29 00:47:10 +0100

    fix on Kick button click and properly name parameters

--- IzvorniKod/frontend/src/components/UserTableRow/UserTableRow.tsx
+++ IzvorniKod/frontend/src/components/UserTableRow/UserTableRow.tsx
@@ -1,34 +1,34 @@
 import { cn } from "../../utils/cn.util";
 
 type UserTableRowType = {
-  userID: number;
+  facultyUserId: number;
   username: string;
   email: string;
   postsReported: number;
   isKicked: boolean;
-  onClick: () => void;
+  onKick: () => void;
 };
 
 export const UserTableRow = ({
-  userID,
+  facultyUserId,
   username,
   email,
   postsReported,
   isKicked,
-  onClick,
+  onKick,
 }: UserTableRowType) => {
   return (
     <div className="bg-white flex justify-between items-center px-4 py-1 scroll-py-14 border-y-gray_border border-y-[1px]">
-      <div>{userID}</div>
+      <div>{facultyUserId}</div>
       <div className="w-[20%] truncate">{username}</div>
       <div className="w-[20%] truncate">{email}</div>
       <div>{postsReported}</div>
       <div
-        onClick={onClick}
+        onClick={onKick}
         className={cn(
           isKicked
-            ? "text-red"
-            : "text-white bg-red rounded-[15px] w-[60px] p-1 text-center"
+            ? "text-red pointer-events-none"
+            : "text-white bg-red rounded-[15px] w-[60px] p-1 text-center cursor-pointer"
         )}
       >
         {isKicked ? "Kicked" : "Kick"}
--- IzvorniKod/frontend/src/pages/Home/Home.tsx
+++ IzvorniKod/frontend/src/pages/Home/Home.tsx
@@ -10,16 +10,16 @@ export const Home = () => {
         subjectDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt..."
         onClick={() => 0}
       />
-      <UserTableRow
-        userID={0}
-        username={"someRandomLongUsernameThatNeedsToBeTruncated"}
-        email={"someEmailthatalsoneedtstobetruncated@gmail.com"}
-        postsReported={1}
-        isKicked={false}
-        onClick={function (): void {
-          throw new Error("Function not implemented.");
-        }}
-      />
+      <div className="w-[70%]">
+        <UserTableRow
+          facultyUserId={0}
+          username={"someRandomLongUsernameThatNeedsToBeTruncated"}
+          email={"someEmailthatalsoneedtstobetruncated@gmail.com"}
+          postsReported={1}
+          isKicked={false}
+          onKick={() => console.log("Kick this player")}
+        />
+      </div>
     </div>
   );
 };
