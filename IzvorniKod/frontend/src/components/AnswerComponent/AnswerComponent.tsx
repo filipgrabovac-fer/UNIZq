d1dc7a3ae4d7f82d18e8ce61a3261de81c3bfd7d
sucicAnamarija <anamarija.sucic@fer.unizg.hr>
2025-01-04 12:20:18 +0100

    Added icons from PostPreviewComponent

--- IzvorniKod/frontend/src/components/AnswerComponent/AnswerComponent.tsx
+++ IzvorniKod/frontend/src/components/AnswerComponent/AnswerComponent.tsx
@@ -1,4 +1,12 @@
-import { UserIcon } from "@heroicons/react/24/solid";
+import {
+  EllipsisVerticalIcon,
+  HandThumbDownIcon,
+  HandThumbUpIcon,
+  HeartIcon,
+  UserIcon,
+} from "@heroicons/react/24/solid";
+import { Popover } from "antd";
+import { useState } from "react";
 
 type AnswerComponentType = {
   answerAuthor: string;
@@ -6,16 +14,96 @@ type AnswerComponentType = {
   onClick: () => void;
 };
 
+type GetIconStyleType = {
+  isClicked: boolean;
+  color: string;
+};
+
+const getIconStyle = ({ color, isClicked }: GetIconStyleType) => {
+  return {
+    stroke: isClicked ? "none" : "black",
+    strokeWidth: isClicked ? "0" : "1px",
+    fill: isClicked ? color : "white",
+  };
+};
+
 export const AnswerComponent = ({
   answerAuthor,
   answerText,
   onClick,
 }: AnswerComponentType) => {
+  const [isHeartClicked, setIsHeartClicked] = useState(false);
+  const [isThumbUpClicked, setIsThumbUpClicked] = useState(false);
+  const [isThumbDownClicked, setIsThumbDownClicked] = useState(false);
+
+  const content = (
+    <div className="flex flex-col gap-3 p-1">
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        className="cursor-pointer"
+      >
+        Report
+      </button>
+
+      {/* napraviti validaciju može li korisnik obrisati ovaj post i ako ne može, onda ne prikazivati Delete gumb */}
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        className="cursor-pointer text-red"
+      >
+        Delete
+      </button>
+    </div>
+  );
+
   return (
     <div className="bg-white">
       <div className="flex">
         <UserIcon className="w-5 mr-[1%]" />
         <p>{answerAuthor}</p>
+        <div className="flex gap-1.5">
+          <HeartIcon
+            onClick={(e) => {
+              e.stopPropagation();
+              setIsHeartClicked(!isHeartClicked);
+            }}
+            className="w-[20px] cursor-pointer"
+            style={getIconStyle({ color: "red", isClicked: isHeartClicked })}
+          />
+          <HandThumbUpIcon
+            onClick={(e) => {
+              e.stopPropagation();
+              setIsThumbUpClicked(!isThumbUpClicked);
+            }}
+            className="w-[20px] cursor-pointer"
+            style={getIconStyle({
+              color: "#111D4A",
+              isClicked: isThumbUpClicked,
+            })}
+          />
+          <HandThumbDownIcon
+            onClick={(e) => {
+              e.stopPropagation();
+              setIsThumbDownClicked(!isThumbDownClicked);
+            }}
+            className="w-[20px] cursor-pointer"
+            style={getIconStyle({
+              color: "#111D4A",
+              isClicked: isThumbDownClicked,
+            })}
+          />
+          <Popover
+            arrow={false}
+            content={content}
+            trigger="click"
+            className="w-[25px]"
+          >
+            <EllipsisVerticalIcon className="cursor-pointer" />
+          </Popover>
+        </div>
       </div>
       <p>{answerText}</p>
     </div>
