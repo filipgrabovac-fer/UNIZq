3fe55edda2466252b5044154e63182f2563d5328
Matej Stupljanec <matej.stupljanec@fer.unizg.hr>
2025-01-05 01:34:28 +0100

    add annotation to AnswerImageRepository

--- IzvorniKod/src/main/java/com/educhat/backend/repository/AnswerImageRepository.java
+++ IzvorniKod/src/main/java/com/educhat/backend/repository/AnswerImageRepository.java
@@ -2,9 +2,11 @@ package com.educhat.backend.repository;
 
 import com.educhat.backend.models.AnswerImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AnswerImageRepository extends JpaRepository<AnswerImage, Long> {
 
     List<AnswerImage> findByAnswerId(Long answerId);
