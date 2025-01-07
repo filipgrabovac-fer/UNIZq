a67c723c6a56ca7ee2de52e3034c0f6a201b9e29
sucicAnamarija <anamarija.sucic@fer.unizg.hr>
2025-01-07 20:46:18 +0100

    Made answers scroll and made post sticky

--- IzvorniKod/frontend/src/pages/Post/Post.component.tsx
+++ IzvorniKod/frontend/src/pages/Post/Post.component.tsx
@@ -4,78 +4,97 @@ import { AddAnswerComponent } from "../../components/SearchComponent/AddAnswerCo
 
 export const Post = () => {
   return (
-    <div>
-      <SelectedPostHeader
-        images={[
-          "https://cdn.pixabay.com/photo/2022/03/27/12/46/chongqing-7094955_640.jpg",
-          "https://cdn.pixabay.com/photo/2020/10/22/10/05/formula-5675604_640.jpg",
-          "https://cdn.pixabay.com/photo/2024/12/05/11/17/fishing-9246365_640.jpg",
-        ]}
-        postAuthor="Post Author"
-        postData="Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore aspernatur architecto provident saepe dicta, quasi fuga consequatur amet quo tempora atque ad, maiores nemo magni tenetur in quos recusandae fugiat?"
-        postName="Post Name"
-      />
-      <AnswerComponent
-        answerAuthor="Answer author"
-        answerText="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quas
-    minus, dolore, laboriosam aliquid natus officia consequatur commodi
-    iusto modi nisi magni, quisquam ducimus rerum ad nostrum itaque nobis
-    similique.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quas
-    minus, dolore, laboriosam aliquid natus officia consequatur commodi
-    iusto modi nisi magni, quisquam ducimus rerum ad nostrum itaque nobis
-    similique."
-        pictures={[
-          "https://cdn.pixabay.com/photo/2022/03/27/12/46/chongqing-7094955_640.jpg",
-          "https://cdn.pixabay.com/photo/2020/10/22/10/05/formula-5675604_640.jpg",
-          "https://cdn.pixabay.com/photo/2024/12/05/11/17/fishing-9246365_640.jpg",
-        ]}
-      />
-      <AnswerComponent
-        answerAuthor="Answer author"
-        answerText="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quas
-    minus, dolore, laboriosam aliquid natus officia consequatur commodi
-    iusto modi nisi magni, quisquam ducimus rerum ad nostrum itaque nobis
-    similique.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quas
-    minus, dolore, laboriosam aliquid natus officia consequatur commodi
-    iusto modi nisi magni, quisquam ducimus rerum ad nostrum itaque nobis
-    similique."
-        pictures={[
-          "https://cdn.pixabay.com/photo/2022/03/27/12/46/chongqing-7094955_640.jpg",
-          "https://cdn.pixabay.com/photo/2020/10/22/10/05/formula-5675604_640.jpg",
-          "https://cdn.pixabay.com/photo/2024/12/05/11/17/fishing-9246365_640.jpg",
-        ]}
-      />
-      <AnswerComponent
-        answerAuthor="Answer author"
-        answerText="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quas
-    minus, dolore, laboriosam aliquid natus officia consequatur commodi
-    iusto modi nisi magni, quisquam ducimus rerum ad nostrum itaque nobis
-    similique.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quas
-    minus, dolore, laboriosam aliquid natus officia consequatur commodi
-    iusto modi nisi magni, quisquam ducimus rerum ad nostrum itaque nobis
-    similique."
-        pictures={[
-          "https://cdn.pixabay.com/photo/2022/03/27/12/46/chongqing-7094955_640.jpg",
-          "https://cdn.pixabay.com/photo/2020/10/22/10/05/formula-5675604_640.jpg",
-          "https://cdn.pixabay.com/photo/2024/12/05/11/17/fishing-9246365_640.jpg",
-        ]}
-      />
-      <AnswerComponent
-        answerAuthor="Answer author"
-        answerText="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quas
-    minus, dolore, laboriosam aliquid natus officia consequatur commodi
-    iusto modi nisi magni, quisquam ducimus rerum ad nostrum itaque nobis
-    similique.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quas
-    minus, dolore, laboriosam aliquid natus officia consequatur commodi
-    iusto modi nisi magni, quisquam ducimus rerum ad nostrum itaque nobis
-    similique."
-        pictures={[
-          "https://cdn.pixabay.com/photo/2022/03/27/12/46/chongqing-7094955_640.jpg",
-          "https://cdn.pixabay.com/photo/2020/10/22/10/05/formula-5675604_640.jpg",
-          "https://cdn.pixabay.com/photo/2024/12/05/11/17/fishing-9246365_640.jpg",
-        ]}
-      />
-      <div className="sticky bottom-0">
+    <div className="flex flex-col h-screen">
+      <div className="flex-none">
+        <SelectedPostHeader
+          images={[
+            "https://cdn.pixabay.com/photo/2022/03/27/12/46/chongqing-7094955_640.jpg",
+            "https://cdn.pixabay.com/photo/2020/10/22/10/05/formula-5675604_640.jpg",
+            "https://cdn.pixabay.com/photo/2024/12/05/11/17/fishing-9246365_640.jpg",
+          ]}
+          postAuthor="Post Author"
+          postData="Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore aspernatur architecto provident saepe dicta, quasi fuga consequatur amet quo tempora atque ad, maiores nemo magni tenetur in quos recusandae fugiat?"
+          postName="Post Name"
+        />
+      </div>
+      <div className="flex-1 overflow-y-scroll">
+        <AnswerComponent
+          answerAuthor="Answer author"
+          answerText="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quas
+minus, dolore, laboriosam aliquid natus officia consequatur commodi
+iusto modi nisi magni, quisquam ducimus rerum ad nostrum itaque nobis
+similique.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quas
+minus, dolore, laboriosam aliquid natus officia consequatur commodi
+iusto modi nisi magni, quisquam ducimus rerum ad nostrum itaque nobis
+similique."
+          pictures={[
+            "https://cdn.pixabay.com/photo/2022/03/27/12/46/chongqing-7094955_640.jpg",
+            "https://cdn.pixabay.com/photo/2020/10/22/10/05/formula-5675604_640.jpg",
+            "https://cdn.pixabay.com/photo/2024/12/05/11/17/fishing-9246365_640.jpg",
+          ]}
+        />
+        <AnswerComponent
+          answerAuthor="Answer author"
+          answerText="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quas
+minus, dolore, laboriosam aliquid natus officia consequatur commodi
+iusto modi nisi magni, quisquam ducimus rerum ad nostrum itaque nobis
+similique.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quas
+minus, dolore, laboriosam aliquid natus officia consequatur commodi
+iusto modi nisi magni, quisquam ducimus rerum ad nostrum itaque nobis
+similique."
+          pictures={[
+            "https://cdn.pixabay.com/photo/2022/03/27/12/46/chongqing-7094955_640.jpg",
+            "https://cdn.pixabay.com/photo/2020/10/22/10/05/formula-5675604_640.jpg",
+            "https://cdn.pixabay.com/photo/2024/12/05/11/17/fishing-9246365_640.jpg",
+          ]}
+        />
+        <AnswerComponent
+          answerAuthor="Answer author"
+          answerText="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quas
+minus, dolore, laboriosam aliquid natus officia consequatur commodi
+iusto modi nisi magni, quisquam ducimus rerum ad nostrum itaque nobis
+similique.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quas
+minus, dolore, laboriosam aliquid natus officia consequatur commodi
+iusto modi nisi magni, quisquam ducimus rerum ad nostrum itaque nobis
+similique."
+          pictures={[
+            "https://cdn.pixabay.com/photo/2022/03/27/12/46/chongqing-7094955_640.jpg",
+            "https://cdn.pixabay.com/photo/2020/10/22/10/05/formula-5675604_640.jpg",
+            "https://cdn.pixabay.com/photo/2024/12/05/11/17/fishing-9246365_640.jpg",
+          ]}
+        />
+        <AnswerComponent
+          answerAuthor="Answer author"
+          answerText="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quas
+minus, dolore, laboriosam aliquid natus officia consequatur commodi
+iusto modi nisi magni, quisquam ducimus rerum ad nostrum itaque nobis
+similique.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quas
+minus, dolore, laboriosam aliquid natus officia consequatur commodi
+iusto modi nisi magni, quisquam ducimus rerum ad nostrum itaque nobis
+similique."
+          pictures={[
+            "https://cdn.pixabay.com/photo/2022/03/27/12/46/chongqing-7094955_640.jpg",
+            "https://cdn.pixabay.com/photo/2020/10/22/10/05/formula-5675604_640.jpg",
+            "https://cdn.pixabay.com/photo/2024/12/05/11/17/fishing-9246365_640.jpg",
+          ]}
+        />
+        <AnswerComponent
+          answerAuthor="Answer author"
+          answerText="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quas
+minus, dolore, laboriosam aliquid natus officia consequatur commodi
+iusto modi nisi magni, quisquam ducimus rerum ad nostrum itaque nobis
+similique.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quas
+minus, dolore, laboriosam aliquid natus officia consequatur commodi
+iusto modi nisi magni, quisquam ducimus rerum ad nostrum itaque nobis
+similique."
+          pictures={[
+            "https://cdn.pixabay.com/photo/2022/03/27/12/46/chongqing-7094955_640.jpg",
+            "https://cdn.pixabay.com/photo/2020/10/22/10/05/formula-5675604_640.jpg",
+            "https://cdn.pixabay.com/photo/2024/12/05/11/17/fishing-9246365_640.jpg",
+          ]}
+        />
+      </div>
+      <div className="flex-none sticky bottom-0 my-2 bg-white">
         <AddAnswerComponent postContent="Explain theory of relativity" />
       </div>
     </div>
