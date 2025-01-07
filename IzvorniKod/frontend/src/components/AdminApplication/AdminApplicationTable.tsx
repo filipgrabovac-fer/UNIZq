2d79c5d21ca39d490f394beb72da5b2d340df097
Matija <matija.pticar@fer.hr>
2025-01-07 21:08:09 +0100

    napravljen admin application page

---
+++ IzvorniKod/frontend/src/components/AdminApplication/AdminApplicationTable.tsx
@@ -0,0 +1,80 @@
+import { AdminApplicationTableRow } from "./AdminApplicationTableRow/AdminApplicationTableRow";
+import { useEffect, useState } from "react";
+
+type AdminApplication = {
+  userId: number;
+  facultyId: number;
+  facultyName: string;
+  userEmail: string;
+  isApproved: boolean;
+  onApprove: (userId: number) => void;
+  userRole: string;
+};
+
+type AdminApplicationTableType = {
+  applications: AdminApplication[];
+};
+
+export const AdminApplicationTable = ({
+  applications: initialApplications,
+}: AdminApplicationTableType) => {
+  const [applications, setApplications] =
+    useState<AdminApplication[]>(initialApplications);
+  // Ensure unique applications by userId and filter out undefined
+  useEffect(() => {
+    const uniqueApplications = Array.from(
+      new Set(initialApplications.map((application) => application.userId))
+    )
+      .map((id) =>
+        initialApplications.find((application) => application.userId === id)
+      )
+      .filter(
+        (application): application is AdminApplication =>
+          application !== undefined
+      );
+
+    setApplications(uniqueApplications);
+  }, [initialApplications]);
+
+  // const handleApprove = (userId: number) => {
+  //   setApplications((prevApplications) =>
+  //     prevApplications.map((application) =>
+  //       application.userId === userId
+  //         ? { ...application, isApproved: true, userRole: "ADMIN" }
+  //         : application
+  //     )
+  //   );
+  //   // Here you can add the logic to update the user's role in the backend
+  //   console.log(`User ${userId} approved as ADMIN`);
+  // };
+
+  return (
+    <div className="border-gray_border border-[1px] rounded-[10px]">
+      {/* Header Row */}
+      <div className="flex items-center p-2">
+        <div className="w-[20%]">userId</div>
+        <div className="w-[20%] truncate">facultyId</div>
+        <div className="w-[20%] truncate">facultyName</div>
+        <div className="w-[20%] text-center truncate">userEmail</div>
+        <p className="w-[10%] text-end">Status</p>
+        <p className="w-[10%] text-end">Role</p>
+      </div>
+
+      {/* Scrollable Rows */}
+      <div className="max-h-[75vh] overflow-y-auto max-md:max-h-[70vh]">
+        {applications.map((application) => (
+          <AdminApplicationTableRow
+            key={application.userId}
+            userId={application.userId}
+            facultyId={application.facultyId}
+            facultyName={application.facultyName}
+            userEmail={application.userEmail}
+            isApproved={application.isApproved}
+            userRole={application.userRole}
+            onApprove={() => application.onApprove(application.userId)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ IzvorniKod/frontend/src/components/AdminApplication/AdminApplicationTableRow/AdminApplicationTableRow.tsx
@@ -0,0 +1,42 @@
+import { cn } from "../../../utils/cn.util";
+type AdminApplicationTableRowType = {
+  userId: number;
+  facultyId: number;
+  facultyName: string;
+  userEmail: string;
+  isApproved: boolean;
+  onApprove: (userId: number) => void;
+  userRole: string;
+};
+
+export const AdminApplicationTableRow = ({
+  userId,
+  facultyId,
+  facultyName,
+  userEmail,
+  isApproved,
+  onApprove,
+  userRole,
+}: AdminApplicationTableRowType) => {
+  return (
+    <div className="bg-white flex items-center px-4 py-1 scroll-py-14 border-t-gray_border border-t-[1px]">
+      <div className="w-[20%]">{userId}</div>
+      <div className="w-[20%] truncate">{facultyId}</div>
+      <div className="w-[20%] truncate">{facultyName}</div>
+      <div className="w-[20%] text-center truncate">{userEmail}</div>
+      <div className="w-[10%] flex justify-end">
+        {isApproved || userRole == "ADMIN" ? (
+          <span className="rounded-full  text-green-500  p-1">Approved</span>
+        ) : (
+          <button
+            onClick={() => onApprove(userId)}
+            className="rounded-full text-white bg-green-500 p-1"
+          >
+            Approve
+          </button>
+        )}
+      </div>
+      <div className="w-[10%] flex justify-end">{userRole}</div>
+    </div>
+  );
+};
