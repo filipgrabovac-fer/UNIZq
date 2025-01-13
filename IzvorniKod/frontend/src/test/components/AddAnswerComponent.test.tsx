31a346df28d2e416ce57fa43c9161e020a9d7c52
Matija <matija.pticar@fer.hr>
2025-01-13 14:57:50 +0100

    popravljen test za AddAnswer component

---
+++ IzvorniKod/frontend/src/test/components/AddAnswerComponent.test.tsx
@@ -0,0 +1,5 @@
+import { test, expect } from "vitest";
+import { AddAnswerComponent } from "../../components/SearchComponent/AddAnswerComponent";
+test("answer component", async () => {
+  expect(AddAnswerComponent).toBeTruthy();
+});
